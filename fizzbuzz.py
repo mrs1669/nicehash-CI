57d3afef8338697de9d7d926cf7a4b7f3d4a7230
Takumi Muraishi <mrs1669.develop@gmail.com>
2022-06-17 13:58:07 +0900

    Update fizzbuzz.py

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -4,10 +4,10 @@ def fizzbuzz(num):
 'FizzBuzz'を返す。
 いずれの倍数でもない場合は受け取った数値をそのまま返す。
 """
-if num % 3 == 0 and num % 5 == 0:
-return 'FizzBuzz'
-if num % 3 == 0:
-return 'Fizz'
-if num % 5 == 0:
-return 'Buzz'
-return num
+  if num % 3 == 0 and num % 5 == 0:
+    return 'FizzBuzz'
+  if num % 3 == 0:
+    return 'Fizz'
+  if num % 5 == 0:
+    return 'Buzz'
+  return num
